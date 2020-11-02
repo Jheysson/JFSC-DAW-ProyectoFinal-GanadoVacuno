3962462ec8faad7ed1f037471fd1f747864c6d55
Jheysson Urbano <jheyssonurb@gmail.com>
2020-11-01 21:59:36 -0500

    adicion de lista ganado hembra y macho

---
+++ src/main/java/pe/unjfsc/daw/spring5/controller/CCGanadoHembra.java
@@ -0,0 +1,32 @@
+package pe.unjfsc.daw.spring5.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import pe.unjfsc.daw.spring5.model.GanadoVacuno;
+import pe.unjfsc.daw.spring5.service.GanadoVacunoService;
+
+@Controller
+public class CCGanadoHembra {
+	
+	private static final Logger log = LoggerFactory.getLogger("CCNatalidad");
+	
+	@Autowired
+	private GanadoVacunoService ganadoVacunoService;
+	
+	
+	@RequestMapping(value = "ganadoVacuno/ListadoGanadoHembra.lhs")
+	public ModelAndView listarGanado() {		
+		ModelAndView model = new ModelAndView();
+		List<GanadoVacuno> list = ganadoVacunoService.getGanadoVacunoHembra();
+		model.addObject("listGanadoHembra",list);
+		model.setViewName("listGanadoHembra");
+		return model;
+	}	
+}
---
+++ src/main/java/pe/unjfsc/daw/spring5/controller/CCGanadoMacho.java
@@ -0,0 +1,31 @@
+package pe.unjfsc.daw.spring5.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import pe.unjfsc.daw.spring5.model.GanadoVacuno;
+import pe.unjfsc.daw.spring5.service.GanadoVacunoService;
+
+@Controller
+public class CCGanadoMacho {
+private static final Logger log = LoggerFactory.getLogger("CCNatalidad");
+	
+	@Autowired
+	private GanadoVacunoService ganadoVacunoService;
+	
+	
+	@RequestMapping(value = "ganadoVacuno/ListadoGanadoMacho.lhs")
+	public ModelAndView listarGanado() {		
+		ModelAndView model = new ModelAndView();
+		List<GanadoVacuno> list = ganadoVacunoService.getGanadoVacunoMacho();
+		model.addObject("listGanadoMacho",list);
+		model.setViewName("listGanadoMacho");
+		return model;
+	}	
+}
