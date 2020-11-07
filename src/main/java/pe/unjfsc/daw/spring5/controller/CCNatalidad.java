61fea1afeb1761d690090f4d7e67486559649d37
borda-2019 <bordanonatocarlos@gmail.com>
2020-11-07 11:24:24 -0500

    "Haciendo un servicio rest para la obtencion de madre en formato json"

--- src/main/java/pe/unjfsc/daw/spring5/controller/CCNatalidad.java
+++ src/main/java/pe/unjfsc/daw/spring5/controller/CCNatalidad.java
@@ -93,6 +93,7 @@ public class CCNatalidad {
     	natalidadService.deletenatalidad(cuia);
     	return new ModelAndView("redirect:/natalidad/ListadoNatalidad"); 
     	
-    }
+    }    
+    
 }
 
---
+++ src/main/java/pe/unjfsc/daw/spring5/controller/RestController.java
@@ -0,0 +1,32 @@
+package pe.unjfsc.daw.spring5.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pe.unjfsc.daw.spring5.model.GanadoVacuno;
+import pe.unjfsc.daw.spring5.model.natalidad;
+import pe.unjfsc.daw.spring5.service.GanadoVacunoService;
+import pe.unjfsc.daw.spring5.service.natalidadService;
+
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	@Autowired
+    private natalidadService natalidadService;
+	
+	@Autowired
+	private GanadoVacunoService ganadoVacunoService;
+	
+	@RequestMapping(value="/buscar/verificarMadre/{cuia}", method=RequestMethod.GET)
+    public natalidad getNatalidad(@PathVariable("cuia") int cuia) {
+		return natalidadService.findnatalidadByCuia(cuia);
+    }
+    	
+	@RequestMapping(value="/buscar/verificarMadreGanadoVacuno/{cuia}", method=RequestMethod.GET)
+    public GanadoVacuno getGanadoVacuno(@PathVariable("cuia") int cuia) {
+		return ganadoVacunoService.findGanadoVacunoByCuia(cuia);
+    }
+}
