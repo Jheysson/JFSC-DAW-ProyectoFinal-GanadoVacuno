4071ee04132ef699d60d82430aac0d0752577e5e
Jheysson Urbano <jheyssonurb@gmail.com>
2020-11-12 03:59:02 -0500

    Añadiendo funcionalidad: Al registrar natalidad, se registra tambien en ganadoVacuno

--- src/main/java/pe/unjfsc/daw/spring5/dao/natalidadDaoImpl.java
+++ src/main/java/pe/unjfsc/daw/spring5/dao/natalidadDaoImpl.java
@@ -1,8 +1,11 @@
 package pe.unjfsc.daw.spring5.dao;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,6 +20,8 @@ import pe.unjfsc.daw.spring5.model.natalidadRowMapper;
 @Transactional
 @Repository
 public class natalidadDaoImpl implements natalidadDao{
+	
+	private static final Logger log = LoggerFactory.getLogger("DAO IMPL NATALIDAD");
 	
 	@Autowired
 	 private JdbcTemplate jdbcTemplate;
@@ -38,12 +43,34 @@ public class natalidadDaoImpl implements natalidadDao{
 
 	@Override
 	public void addnatalidad(natalidad pnatalidad) {
+		log.info("LLEGANDO SEXO: "+pnatalidad.getIdSexo());
 		jdbcTemplate.update(CDConstanteSQLNatalidad.SQL_INSERT, 
 				pnatalidad.getCuiaNata(),
 				pnatalidad.getIdSexo(),
 				pnatalidad.getIdGeno(),
 				pnatalidad.getFechNaciNata(),
 				pnatalidad.getPesoNata(),
+				pnatalidad.getCuiaMadr(),
+				pnatalidad.getCuiaPadr(),
+				pnatalidad.getCodiProp(),
+				pnatalidad.getObse(),
+				pnatalidad.getFoto()
+				);
+		jdbcTemplate.update(CDConstanteSQLNatalidad.SQL_INSERT_REGISTRO_GANADO,
+				pnatalidad.getCuiaNata(),
+				"",
+				pnatalidad.getIdSexo(),				
+				4, //Estado activo N.A
+				1, //Estado animal Normal,
+				asignarTipo(pnatalidad.getIdSexo()),
+				0, //Cantidad de partos,
+				2, //Origen 2 = Autoctono
+				pnatalidad.getIdGeno(),
+				pnatalidad.getFechNaciNata(),
+				calcularEdad(pnatalidad.getFechNaciNata()),
+				calcularEtapa(calcularEdad(pnatalidad.getFechNaciNata()), pnatalidad.getIdSexo()),
+				pnatalidad.getPesoNata(),
+				0,
 				pnatalidad.getCuiaMadr(),
 				pnatalidad.getCuiaPadr(),
 				pnatalidad.getCodiProp(),
@@ -74,5 +101,44 @@ public class natalidadDaoImpl implements natalidadDao{
 		jdbcTemplate.update(CDConstanteSQLNatalidad.SQL_DELETE, cuia);
 		
 	}
-
+	
+	protected int calcularEdad(String fechNaci) {
+		int edad = (int) ChronoUnit.MONTHS.between(LocalDate.parse(fechNaci), LocalDate.now());
+		return edad;
+	}
+	
+	protected String calcularEtapa(int edad, String sexo) {
+		String etapa="";
+		if (sexo.equals("1")) { //Hembra
+			if(edad <= 4) {
+				etapa = "Ternera";
+			}else if(edad > 4 && edad <=12) {
+				etapa = "Destete";
+			}else if(edad > 12 && edad <=24) {
+				etapa = "Becerra";
+			}else if(edad >= 24) {
+				etapa = "Vaca Adulta";
+			}		
+		}else { //Macho
+			if(edad <= 4) {
+				etapa = "Ternero";
+			}else if(edad > 4 && edad <=12) {
+				etapa = "Destete";
+			}else if(edad > 12 && edad <=24) {
+				etapa = "Becerro";
+			}else if(edad >= 24) {
+				etapa = "Toro Adulto";
+			}		
+		}		
+		return etapa;
+	}
+	protected int asignarTipo(String sexo) {
+		int tipo = 0;
+		if (sexo.equals("1")) {
+			tipo = 1; //Leche
+		}else {
+			tipo = 2; //Carne
+		}
+		return tipo;
+	}
 }
--- src/main/java/pe/unjfsc/daw/spring5/model/CDConstanteSQLNatalidad.java
+++ src/main/java/pe/unjfsc/daw/spring5/model/CDConstanteSQLNatalidad.java
@@ -9,4 +9,9 @@ public class CDConstanteSQLNatalidad {
 	public static final String SQL_DELETE = "UPDATE " + SQL_TABLE + " SET esta = 0 WHERE cuiaNata = ?";
 	public static final String SQL_SELECT_ALL = "SELECT * FROM vistanatalidad";
 	public static final String SQL_SELECT_BY_ID = "SELECT * FROM natalidad WHERE cuiaNata = ?";
+	
+	//Insertar a registroganado
+	public static final String SQL_TABLE_REGISTRO_GANADO = "registroganado";
+	public static final String SQL_INSERT_REGISTRO_GANADO = "INSERT INTO "+SQL_TABLE_REGISTRO_GANADO+" (cuiaGana, aliaGana, idSexo, idEstaActi, idEstaAnim, idTipoGana, cantPartGana, idOrig, idGeno, fechNaciGana, edadGana, etapaGana, pesoGana, tallGana, cuiaMadr, cuiaPadr, codiPropInse, obse, imagen)"
+			+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";			
 }
